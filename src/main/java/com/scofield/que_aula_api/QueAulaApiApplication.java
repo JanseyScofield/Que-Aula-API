be8034d5d2aff404854f7c8c0abb77423ec28aa3
JanseyScofield <janscofield10@gmail.com>
2025-02-03 10:10:05 -0300

    Começando o projeto montando a arquitetura padrão do Spring Boot

---
+++ src/main/java/com/scofield/que_aula_api/QueAulaApiApplication.java
@@ -0,0 +1,13 @@
+package com.scofield.que_aula_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueAulaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueAulaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scofield/que_aula_api/QueAulaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scofield.que_aula_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueAulaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
